7256c99d608ab068f73d5645b72553cfcfbe3228
Charlley <charlley.g.l@gmail.com>
2024-10-24 02:03:49 +0800

    first commit of 4SPIRW branch

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # USB2SPI-AutoControl
-Automaticly control USB2SPI.exe to read data from bin file and write data to specific device.
+Automaticly control SPI_RW.exe to read data from bin file and write data to specific device.
 
 # Environment
 
-## 1. Install USB2SPI.
+## 1. Prepare SPI_RW.exe
 
-A release version is already in this repository.
+You should compile the source code of SPI_RW.exe . You can find the source code at: **'./Release-2024-08-19/demo程序/SPI_RW_VS2019 For VC-2024-08-10-R2.zip'**
 
 ## 2. See 'Attention' in the front of the demo.py
 
 ## 3. Define your own function which can 'Read data from bin file'
+
+## 4. Define your own code
+
+This script only provide you demos. Define your own code in **auto_job()**
 
 # How to Run
 
--- demo.py
+++ demo.py
@@ -5,25 +5,21 @@
         2. Use pip to install pywinauto Python Lib.
         3. pywin32 Pythyon Lib in pip repository which is required by pywinauto is unusable.
         4. Reinstall pywin32 from release exe insted of pip. Site: https://github.com/mhammond/pywin32/releases
-        5. Don't start 'USB2SPI.exe' before this script. This script will automaticly start 'USB2SPI.exe'.
+        5. Don't start 'SPI_RW.exe' before this script. This script will automaticly start 'SPI_RW.exe'.
     @What does this script do?
-        1. Start and link a process from the source file of 'USB2SPI.exe'.
+        1. Start and link a process from the source file of 'SPI_RW.exe'.
         2. Access the window of the process.
-        3. Automaticly click the button to achieve our goal:
-            A. 'Connect' - Connect to hardware.
-            B. Loop of Write-text-to-Write_Area and 'Do write' - Our main job, loading data from bin file and write data to device.
+        3. Automaticly click the button to achieve our goal
 '''
 
 from pywinauto import Application
 from pywinauto.keyboard import send_keys
 import io
 import sys
-import re
 import time
-import os
 
 # 通过可执行文件路径启动并连接应用程序
-app = Application(backend='uia').start(r'C:\Program Files (x86)\ONYIB\USB2SPI\bin\USB2SPI.exe')
+app = Application(backend='win32').start(r'C:\Users\charl\Desktop\projecthjq\SPI_RW_V02\SPI_RW\Debug\SPI_RW.exe')
 
 # 获取主窗口
 main_window = app.window()
@@ -35,12 +31,36 @@ main_window.print_control_identifiers()
 sys.stdout = sys.__stdout__
 identifiers_output = output.getvalue()
 
+# 通过查看identifiers_output的内容，来寻找自己需要操作的控件的编号
+# print(identifiers_output)
+
+# 先连接设备
+connect_button = main_window['连接']
+connect_button.click()
+
+def get_edit_content(app, edit_title):
+    # 获取主窗口
+    main_window = app.window()
+    
+    # 查找 Edit 控件
+    edit_control = main_window.child_window(title=edit_title, control_type="Edit")
+    
+    # 获取并返回 Edit 中的文本内容
+    return edit_control.get_value()
+
+def load_file(app):
+    """
+    Fail to develop
+    Cannot connect to Windows file select function
+    So you should read bin file by python code and directly send bin file content to 'Edit' type identifier, then click write-button
+    """
+
 def extract_auto_id(output):
     # 将输出按行分割
     lines = output.splitlines()
     auto_id = None
 
-    # 遍历每一行，找到包含 ['Edit4'] 的行(Write Area的输入框控件编号是Edit4)
+    # 遍历每一行，找到包含 ['Edit4'] 的行(Write Area的输入框控件编号是Edit4)，数字4只是一个demo，实际需要查看identifiers_output找到自己的EditX
     for i, line in enumerate(lines):
         if "['Edit4']" in line:
             # 找到 ['Edit4'] 后，检查下一行是否包含 auto_id
@@ -57,41 +77,41 @@ def extract_auto_id(output):
         print("auto_id not found for Edit4")
     return auto_id
 
-auto_id = extract_auto_id(identifiers_output)
-
-if auto_id == None:
-    print('auto_id not found')
-    os.exit(0)
-
-# 先连接设备
-main_window['Connect'].click()
-print("11")
-edit_control = main_window.child_window(control_type="Edit", found_index=0)
-
 def read_data_from_bin_file():
     '''
     Define your function that read your bin file.
     '''
     return "aa"
 
-# 每隔 20 秒点击按钮的函数
-def auto_click_buttons():
-    while True:
+def auto_job(app, identifiers_output):
+    """ Demo 1: How to get value of a 'Edit' type identifier
+    edit_content = get_edit_content(app, '值：')
+    print("Edit 中的内容:", edit_content)
+    """
 
-        # 使用 send_keys() 方法将文件路径输入到文件名文本框中
-        time.sleep(2)
-        command_edit = main_window.child_window(auto_id=auto_id, control_type="Edit")
-        command_edit.set_focus()
-        time.sleep(0.5)
-        data = read_data_from_bin_file()
-        send_keys(data)
+    """ Demo 2: How to click a button
+    connect_button = main_window['载入']
+    connect_button.click()
+    """
 
-        # 点击 'Write to device' 按钮
-        main_window['Do write'].click()
-        print('Clicked Write to device button')
-        
-        # 每 20 秒重复一次
-        time.sleep(20)
+    """ Demo 3: How to input content to a 'Edit' type identifier
+    auto_id = extract_auto_id(identifiers_output)
+    command_edit = main_window.child_window(auto_id=auto_id, control_type="Edit")
+    command_edit.set_focus()
+    time.sleep(0.5)
+    data = read_data_from_bin_file()
+    send_keys(data)
+    """
 
-# 调用函数开始自动点击
-auto_click_buttons()
+    """
+    So, you should:
+    1. Use demo 1 to get value from '值:', so you can write 'if-else' code to decide what to do
+        You can use demo 1 many times, so you can get the value-changed-action to direct what to do
+    2. Use demo 2 to click the buttons that you need
+        Whatever: '连接，载入，执行，清除，blablabla'
+    3. Use demo 3 to get bin file content.
+        We cannot connect to Windows file select function, so you should directly write data to Edit
+        You should edit the source code of SPI_RW.exe, in order to let Edit-identifier's content can be write by click '执行'
+    """
+
+auto_job(app, identifiers_output)
